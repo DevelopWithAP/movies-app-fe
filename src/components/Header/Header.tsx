b53414c36ff18e3686243f0100440414f5e77dfe
Alex <vytautas.rud@gmail.com>
2022-06-28 13:52:12 +0100

    Removed anonymous function && changed prop name in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,15 +6,14 @@ import styles from './Header.module.css';
 
 const Header = (): JSX.Element => {
   const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    setIsActive((isActive) => !isActive);
+  };
   return (
     <header className={styles.header}>
       <MyMoviesLogo className={styles.headerIcon} />
-      <HamburgerButton
-        isActive={isActive}
-        onClick={() => {
-          setIsActive((isActive) => !isActive);
-        }}
-      />
+      <HamburgerButton isActive={isActive} onClick={() => toggleActive()} />
     </header>
   );
 };
