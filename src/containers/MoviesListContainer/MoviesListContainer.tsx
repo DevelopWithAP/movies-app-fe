08172b7225d13119a115c51d645a79e65aead80c
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-07-05 16:05:33 +0300

    Movies List Container completed.

---
+++ src/containers/MoviesListContainer/MoviesListContainer.tsx
@@ -0,0 +1,7 @@
+import styles from './MoviesListContainer.module.css';
+
+const MoviesListContainer = (): JSX.Element => {
+  return <div className={styles.MoviesListContainerStyle}>Movies List</div>;
+};
+
+export default MoviesListContainer;
